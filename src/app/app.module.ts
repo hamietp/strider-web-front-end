8f5f0b39c3b25f404a83794e97030822fb1b8e40
unknown <mirtowow@gmail.com>
2022-07-26 16:41:56 -0300

    feature: initialized home-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
