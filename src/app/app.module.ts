667bbdb01b51b5c21cca056ede83c777f8f1c2c0
unknown <mirtowow@gmail.com>
2022-07-27 15:02:12 -0300

    feature: added components folder with reusable components for posts and profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,42 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { UserPostsComponent } from './components/user-posts/user-posts.component';
+
+const MATERIAL_MODULES = [
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
-    HomePageComponent
+    HomePageComponent,
+    NewPostComponent,
+    UserCardComponent,
+    UserPostsComponent,
   ],
   imports: [
+    ...MATERIAL_MODULES,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
